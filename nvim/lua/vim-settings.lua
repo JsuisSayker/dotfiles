76931f615babb14d2b7bc336c84c5ffbc85597e7
JsuisSayker <killian.trouve@epitech.eu>
2024-11-10 18:44:28 +0100

    [UPD] nvim config
    add the autopairs plugin and the snippet

--- nvim/lua/vim-settings.lua
+++ nvim/lua/vim-settings.lua
@@ -3,6 +3,9 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set smartindent")
+vim.cmd("set autoindent")
 vim.cmd("set number")
 vim.keymap.set("n", "<Space>", "<Nop>")
 vim.g.mapleader = " "
+
+vim.opt.swapfile = false
