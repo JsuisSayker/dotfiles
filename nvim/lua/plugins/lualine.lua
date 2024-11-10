5b26fc10b49a928963901276092763c45b33ead2
JsuisSayker <killian.trouve@epitech.eu>
2024-11-10 21:01:37 +0100

    [UPD] nvim config
    change the themery config to set the catppuccin theme past 8PM and set the tokyonight day during the day

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,10 +1,26 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "dracula",
-			},
-		})
-	end,
+    "nvim-lualine/lualine.nvim",
+    config = function()
+        local function set_lualine_theme()
+            local current_theme = vim.g.colors_name
+            if current_theme == "catppuccin-macchiato" then
+                require("lualine").setup({
+                    options = { theme = "dracula" },
+                })
+            elseif current_theme == "tokyonight" then
+                require("lualine").setup({
+                    options = { theme = "ayu_light" },
+                })
+            end
+        end
+
+        set_lualine_theme()
+
+        vim.api.nvim_create_autocmd("ColorScheme", {
+            pattern = "*",
+            callback = function()
+                set_lualine_theme()
+            end,
+        })
+    end,
 }
--- nvim/lua/plugins/themery.lua
+++ nvim/lua/plugins/themery.lua
@@ -1,4 +1,34 @@
 return {
 	"zaldih/themery.nvim",
 	lazy = false,
+	config = function()
+		local themery = require("themery")
+
+		themery.setup({
+			themes = { "tokyonight", "catppuccin-macchiato" },
+			livePreview = true,
+		})
+
+		local function set_theme_based_on_time()
+			local hour = tonumber(os.date("%H"))
+			if hour >= 7 and hour < 20 then
+				themery.setThemeByName("tokyonight", true)
+                vim.g.colors_name = "tokyonight"
+			else
+				themery.setThemeByName("catppuccin-macchiato", true)
+                vim.g.colors_name = "catppuccin-macchiato"
+			end
+		end
+
+		set_theme_based_on_time()
+
+		vim.api.nvim_create_autocmd("VimEnter", {
+			pattern = "*",
+			callback = function()
+				vim.defer_fn(function()
+					set_theme_based_on_time()
+				end, 0)
+			end,
+		})
+	end,
 }
